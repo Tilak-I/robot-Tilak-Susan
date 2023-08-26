f31e54633b48849b09bdec76b3edb32b05f9d9c3
Tilak-I <iyerbros19@gmail.com>
2023-08-26 12:28:31 -0500

    Added Compressor

--- LevelDos/src/main/java/frc/robot/subsystems/Intake.java
+++ LevelDos/src/main/java/frc/robot/subsystems/Intake.java
@@ -4,8 +4,9 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -16,26 +17,42 @@ public class Intake extends SubsystemBase {
   final DoubleSolenoid solenoid1;
   final DoubleSolenoid solenoid2;
   final WPI_TalonSRX intakeMotor;
+  final Compressor pcmCompressor;
+
   /** Creates a new Intake. */
   public Intake()
    {  
     solenoid1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0,1);
     solenoid2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2,3);
-    intakeMotor = new WPI_TalonSRX(IntakeMotor.kIntakeMotorPort);
-  
-
+    intakeMotor = new WPI_TalonSRX(16);
+    pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+    
    }
-   
+   public void startRollerIntake()
+   { 
+    if (pcmCompressor.isEnabled())
+    {
+      intakeMotor.set(ControlMode.PercentOutput, 1);
+    }
+    //else{pcmCompressor.enable()} Enable it once Ik what the method is
+   }
+   public void endRollerIntake()
+   {
+    intakeMotor.set(ControlMode.PercentOutput, 0);
+    intakeMotor.disable();
+   }
    public void SolenoidExtend()
    {
     solenoid1.set(kForward);
     solenoid2.set(kForward);
+    startRollerIntake();
    }
 
    public void SolenoidRetract()
    {
     solenoid1.set(kReverse);
     solenoid2.set(kReverse);
+    endRollerIntake();
    }
 
   @Override
